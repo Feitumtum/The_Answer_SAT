c79cdde6f1a7a6bbf9b4e6fa121dad3310ce4e49
Crystal Lin <crystalball2000@gmail.com>
2024-04-06 20:14:29 -0700

    make SAT template its own function

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function getAndSetVal() {
   var teacher1 = document.getElementById("teacher1").value;
   var teacher2 = document.getElementById("teacher2").value;
 
-  // Determine pronounsgit status based on student gender
+  // Determine pronouns based on student gender
   if (gender == "Female") {
     var Ugender1 = "She";
     var Lgender1 = "she";
@@ -33,6 +33,30 @@ function getAndSetVal() {
     class_location = `recently attended on ${date} at ${school}`;
   }
 
+  get_SAT_Template(
+    Sname,
+    Lname,
+    title,
+    Ugender1,
+    Lgender1,
+    gender2,
+    teacher1,
+    teacher2,
+    class_location
+  );
+}
+
+function get_SAT_Template(
+  Sname,
+  Lname,
+  title,
+  Ugender1,
+  Lgender1,
+  gender2,
+  teacher1,
+  teacher2,
+  class_location
+) {
   fetch(
     "https://raw.githubusercontent.com/Feitumtum/The_Answer_SAT/Add-New-Templates/DigitalSATTemplate.txt"
   )
@@ -41,58 +65,27 @@ function getAndSetVal() {
       alert("Changed Applied!");
 
       // Exchange the variables
-      data = data.replace(/{Sname}/g, Sname);
-      data = data.replace(/{Lname}/g, Lname);
-      data = data.replace(/{title}/g, title);
-      data = data.replace(/{Ugender1}/g, Ugender1);
-      data = data.replace(/{Lgender1}/g, Lgender1);
-      data = data.replace(/{gender2}/g, gender2);
-      data = data.replace(/{teacher1}/g, teacher1);
-      data = data.replace(/{teacher2}/g, teacher2);
-      data = data.replace(/{recently attended}/g, class_location);
+      data = replacePlaceholders(
+        data,
+        Sname,
+        Lname,
+        title,
+        Ugender1,
+        Lgender1,
+        gender2,
+        teacher1,
+        teacher2,
+        class_location
+      );
 
       // Retain paragraph structure
-      formattedData = data.replace(/\n/g, "<br>");
+      var formattedData = data.replace(/\n/g, "<br>");
 
       // Make Paragraph headings bold
-      bold_sub_headings = formattedData.replace(
-        /(Let’s Be Friends on Socials!|The Answer Class Affordable College Admissions Support)/g,
-        "<strong>$1</strong>"
-      );
+      var bold_sub_headings = formatHeadings(formattedData);
 
       // Adding Hyperlinks
-      hyperlinked_text = bold_sub_headings.replace(
-        "College Admissions Crash Courses",
-        '<a href="https://theanswerclass.com/college-admissions-crash-course-for-students/">College Admissions Crash Courses</a>'
-      );
-      hyperlinked_text = hyperlinked_text.replace(
-        "College Essay Writing Workshops",
-        '<a href="https://theanswerclass.com/college-application-personal-essay-writing-workshops/">College Essay Writing Workshops</a>'
-      );
-      hyperlinked_text = hyperlinked_text.replace(
-        "Personalized College List Service",
-        '<a href="https://theanswerclass.com/personalized-college-list-service/">Personalized College List Service</a>'
-      );
-      hyperlinked_text = hyperlinked_text.replace(
-        "1:1 College Admissions Support for Parents",
-        '<a href="https://theanswerclass.com/college-admissions-crash-courses-and-11-college-application-support/#one-one-college-support-parents">1:1 College Admissions Support for Parents</a>'
-      );
-      hyperlinked_text = hyperlinked_text.replace(
-        "1:1 College Application & Accountability Support for Students",
-        '<a href="https://theanswerclass.com/college-admissions-crash-courses-and-11-college-application-support/#one-one-college-application-for-students">1:1 College Application & Accountability Support for Students</a>'
-      );
-      hyperlinked_text = hyperlinked_text.replace(
-        "premium bundle",
-        '<a href="https://theanswerclass.com/answer-satact-combo-package/">premium bundle</a>'
-      );
-      hyperlinked_text = hyperlinked_text.replace(
-        "Facebook",
-        '<a href="https://www.facebook.com/theanswerclass/?ref=hl">Facebook</a>'
-      );
-      hyperlinked_text = hyperlinked_text.replace(
-        "Instagram",
-        '<a href="https://www.instagram.com/theanswerclass/">Facebook</a>'
-      );
+      var hyperlinked_text = addHyperlinks(bold_sub_headings);
 
       // Display the text onto the website
       document.getElementById("OUTPUT").innerHTML = hyperlinked_text;
@@ -101,6 +94,73 @@ function getAndSetVal() {
         .appendChild(document.getElementById("logo"));
     })
     .catch((error) => console.error("Error fetching file:", error));
+}
+
+function replacePlaceholders(
+  data,
+  Sname,
+  Lname,
+  title,
+  Ugender1,
+  Lgender1,
+  gender2,
+  teacher1,
+  teacher2,
+  class_location
+) {
+  return data
+    .replace(/{Sname}/g, Sname)
+    .replace(/{Lname}/g, Lname)
+    .replace(/{title}/g, title)
+    .replace(/{Ugender1}/g, Ugender1)
+    .replace(/{Lgender1}/g, Lgender1)
+    .replace(/{gender2}/g, gender2)
+    .replace(/{teacher1}/g, teacher1)
+    .replace(/{teacher2}/g, teacher2)
+    .replace(/{recently attended}/g, class_location);
+}
+
+function formatHeadings(data) {
+  return data.replace(
+    /(Let’s Be Friends on Socials!|The Answer Class Affordable College Admissions Support)/g,
+    "<strong>$1</strong>"
+  );
+}
+
+function addHyperlinks(data) {
+  return data
+    .replace(
+      "College Admissions Crash Courses",
+      '<a href="https://theanswerclass.com/college-admissions-crash-course-for-students/">College Admissions Crash Courses</a>'
+    )
+    .replace(
+      "College Essay Writing Workshops",
+      '<a href="https://theanswerclass.com/college-application-personal-essay-writing-workshops/">College Essay Writing Workshops</a>'
+    )
+    .replace(
+      "Personalized College List Service",
+      '<a href="https://theanswerclass.com/personalized-college-list-service/">Personalized College List Service</a>'
+    )
+    .replace(
+      "1:1 College Admissions Support for Parents",
+      '<a href="https://theanswerclass.com/college-admissions-crash-courses-and-11-college-application-support/#one-one-college-support-parents">1:1 College Admissions Support for Parents</a>'
+    )
+    .replace(
+      "1:1 College Application & Accountability Support for Students",
+      '<a href="https://theanswerclass.com/college-admissions-crash-courses-and-11-college-application-support/#one-one-college-application-for-students">1:1 College Application & Accountability Support for Students</a>'
+    )
+    .replace(
+      "premium bundle",
+      '<a href="https://theanswerclass.com/answer-satact-combo-package/">premium bundle</a>'
+    )
+    .replace(
+      "Facebook",
+      '<a href="https://www.facebook.com/theanswerclass/?ref=hl">Facebook</a>'
+    )
+    .replace(
+      "Instagram",
+      '<a href="https://www.instagram.com/theanswerclass/">Facebook</a>'
+    );
 }
 
 function copyText() {
